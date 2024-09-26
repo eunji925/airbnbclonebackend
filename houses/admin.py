2a93600e801ed0c8362c7548866e1f3617da5671
ejlee925 <ejlee.github@gmail.com>
2024-09-26 17:08:55 +0900

    Django Apps

---
+++ houses/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import House
+
+@admin.register(House)
+class HouseAdmin(admin.ModelAdmin):
+
+    # 묶어 보기
+    fields = ("name", "address", ("price_per_night", "pets_allowed"))
+    # admin 패널에 보이고 싶은 column 들의 list
+    list_display = ("name", "price_per_night", "address", "pets_allowed",)
+    # filtering
+    list_filter = ("price_per_night", "pets_allowed")
+    # 검색 
+    search_fields = ("address",) # search_fields = ("address__startswith") : 검색하는 텍스트로 시작하는 것만 검색
+    # list 에서 선택 시 이동하게 만들기
+    list_display_links = ("name", "address",)
+    # list화면에서 바로 list의 property 들을 수정할 수 있다.
+    list_editable = ("pets_allowed",) 
---
+++ houses/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class House(models.Model):
+
+    """ Model Definition for Houses"""
+
+    name = models.CharField(max_length=140) # 텍스트
+    price_per_night = models.PositiveIntegerField(
+        verbose_name="price", help_text="Positive Numbers only"
+    ) # 양수
+    description = models.TextField() # CharField 보다 긴 텍스트
+    address = models.CharField(max_length=140)
+    pets_allowed = models.BooleanField(
+        default=True, help_text= "Does this house allow pets?"
+    ) # CheckBox
+
+    # house class의 string method의 형태를 설정한다.
+    def __str__(self):
+        return self.name 
