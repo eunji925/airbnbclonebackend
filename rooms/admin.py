ea1f25f16170ec3afb3bdafd03e6d3e14aaabf62
ejlee925 <ejlee.github@gmail.com>
2024-09-30 19:25:16 +0900

    room admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Room, Amenity
 
-# Register your models here.
+@admin.register(Room) # Room Model 의 admin을 컨트롤 한다.
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "kind", "owner", "created_at", "updated_at",)
+    list_filter = ("country", "city", "pet_friendly", "kind", "amenities", "created_at", "updated_at",)
+
+@admin.register(Amenity)
+class AmenityAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "created_at", "updated_at", )
+    # 수정 내역에서 해당 필드를 readonly 로 볼 수 있다.
+    readonly_fields = ("created_at", "updated_at",)
--- rooms/models.py
+++ rooms/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from common.models import CommonModel
 
-class Room(models.Model):
+class Room(CommonModel):
     """ Room Model Definition """
 
     class RoomKindChoices(models.TextChoices):
@@ -8,6 +9,7 @@ class Room(models.Model):
         PRIVATE_ROOM = ("private_room", "Private Room")
         SHARED_ROOM = ("shared_room", "Shared Room")
     
+    name = models.CharField(max_length=180, default="")
     country = models.CharField(max_length=50, default="한국")
     city = models.CharField(max_length=80, default="서울")
     price = models.PositiveIntegerField()
@@ -18,8 +20,19 @@ class Room(models.Model):
     pet_friendly = models.BooleanField(default=True)
     kind = models.CharField(max_length=20, choices=RoomKindChoices.choices,)
     owner = models.ForeignKey("users.User",on_delete=models.CASCADE)
+    amenities = models.ManyToManyField("rooms.Amenity",)
 
-class Amenity(models.Model):
+    def __str__(self) -> str:
+        return self.name    
+
+class Amenity(CommonModel):
     """Amenity Definiton"""
     name = models.CharField(max_length=150,)
-    description = models.CharField(max_length=150,null=True,)
+    description = models.CharField(max_length=150,null=True,blank=True)
+        # null = True : 데이터베이스 입장 / blank = True : Django 입장
+    
+    def __str__(self) -> str:
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "Amenities"
