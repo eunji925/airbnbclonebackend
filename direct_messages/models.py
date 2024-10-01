f4ccc89e75ac8d20bff4068a77cbb474504c2c95
ejlee925 <ejlee.github@gmail.com>
2024-10-01 19:36:21 +0900

    related_name

--- direct_messages/models.py
+++ direct_messages/models.py
@@ -4,7 +4,7 @@ from common.models import CommonModel
 class ChattingRoom(CommonModel):
 
     """ Room Model Definition """
-    users = models.ManyToManyField("users.User",)
+    users = models.ManyToManyField("users.User", related_name="chatting_rooms",)
 
     def __str__(self):
         return "Chatting Room"
@@ -13,8 +13,8 @@ class Message(CommonModel):
 
     """ Message Model Definition """
     text = models.TextField()
-    user = models.ForeignKey("users.User", null=True, blank=True, on_delete=models.SET_NULL,)
-    ChattingRoom = models.ForeignKey("direct_messages.ChattingRoom", on_delete=models.CASCADE,)
+    user = models.ForeignKey("users.User", null=True, blank=True, on_delete=models.SET_NULL, related_name="messages",)
+    ChattingRoom = models.ForeignKey("direct_messages.ChattingRoom", on_delete=models.CASCADE, related_name="messages",)
 
     def __str__(self):
         return f"{self.user} says: {self.text}"
