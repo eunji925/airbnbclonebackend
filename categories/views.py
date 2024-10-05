d91af099b3b077002d3bddd0d7aeb9431af27e43
ejlee925 <ejlee.github@gmail.com>
2024-10-05 20:45:38 +0900

    serializer

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Category
+from .serializer import CategorySerializer
 
-# Create your views here.
+@api_view()
+def categories(request):
+    all_categories = Category.objects.all()
+    serializer = CategorySerializer(all_categories, many=True)
+    return Response(serializer.data,)
