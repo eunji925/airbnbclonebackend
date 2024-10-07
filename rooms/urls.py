40c8c1043e471d12a9aa56c74f7d98aa04fc40b5
ejlee925 <ejlee.github@gmail.com>
2024-10-07 16:20:48 +0900

    amenities

--- rooms/urls.py
+++ rooms/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.see_all_rooms), # root 경로\
-    path("<int:room_pk>", views.see_one_room)
+    # templetes 사용시의 url
+    # path("", views.see_all_rooms), # root 경로\
+    # path("<int:room_pk>", views.see_one_room)
     # <> : 우리가 받을 parameter의 데이터 타입:이름(원하는 것으로 지정 가능)
+    path("amenities/", views.Amenities.as_view()),
+    path("amenities/<int:pk>", views.AmenityDetail.as_view()),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,19 +1,78 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Room
+from rest_framework.views import APIView
+from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+from .models import Amenity
+from .serializers import AmenitySerializer
 
-# Create your views here.
-def see_all_rooms(request):
-    rooms = Room.objects.all()
-    return render(
-        request, 
-        "all_rooms.html",
-        {"rooms" : rooms, "title": "this title comes from django!"}
-    )
+# /api/v1/rooms/amenities
+class Amenities(APIView):
 
-def see_one_room(request, room_pk):
-    try:
-        room = Room.objects.get(pk=room_pk)
-        return render(request, "room_detail.html", {"room" : room})
-    except Room.DoesNotExist:
-        return render(request, "room_detail.html", {"not_found" : True})
+    def get(self, request):
+        all_amenities = Amenity.objects.all()
+        serializer = AmenitySerializer(all_amenities, many=True,)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = AmenitySerializer(data = request.data)
+        if serializer.is_valid():
+            amenity = serializer.save()
+            return Response(AmenitySerializer(amenity).data,)
+        else:
+            return Response(serializer.errors)
+
+
+# /api/v1/rooms/amenities/1
+class AmenityDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Amenity.objects.get(pk=pk)
+        except Amenity.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, pk):
+        amenity = self.get_object(pk)
+        serializer = AmenitySerializer(amenity)
+        return Response(serializer.data)
+        # 위처럼 쓰거나 아래처럼 1줄이거나 결국 같은 의미이다.
+        # return Response(AmenitySerializer(self.get_object(pk),).data)
+
+    def put(self, request, pk):
+        amenity = self.get_object(pk)
+        serializer =  AmenitySerializer(amenity, data = request.data, partial = True)
+        # partial = True : 사용자가 일부만 업데이트 할 수 있다.
+        if serializer.is_valid():
+            updated_amenity = serializer.save()
+            return Response(AmenitySerializer(updated_amenity).data,)
+        else:
+            return Response(serializer.errors)
+
+    def delete(self, request, pk):
+        amenity = self.get_object(pk)
+        amenity.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
+
+
+
+
+# templetes 사용하기
+# from django.shortcuts import render
+# from django.http import HttpResponse
+# from .models import Room
+
+# # Create your views here.
+# def see_all_rooms(request):
+#     rooms = Room.objects.all()
+#     return render(
+#         request, 
+#         "all_rooms.html",
+#         {"rooms" : rooms, "title": "this title comes from django!"}
+#     )
+
+# def see_one_room(request, room_pk):
+#     try:
+#         room = Room.objects.get(pk=room_pk)
+#         return render(request, "room_detail.html", {"room" : room})
+#     except Room.DoesNotExist:
+#         return render(request, "room_detail.html", {"not_found" : True})
